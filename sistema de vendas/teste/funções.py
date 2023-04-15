5b75b11a360b6d792b77ac903d2fbca79dbde8e3
Vih2500 <vitoriamaria.rodrigues02@gmail.com>
2023-04-14 23:52:03 -0300

    Deletando códigos antigos

--- sistema de vendas/teste/funções.py
+++
@@ -1,8 +0,0 @@
-import PySimpleGUI as sg
-import bd.conect as ct
-conexao = ct.Connection()
-lista_produtos = []
-def listagem(tabela):
-    pessoas = conexao.query(f'select * from {tabela}')
-    for id, nome, preco in pessoas:
-        lista_produtos.append(f'{id}: {nome} preço: {preco}')
